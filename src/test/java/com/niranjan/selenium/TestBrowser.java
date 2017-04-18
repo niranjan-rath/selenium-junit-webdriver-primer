b4fbb19f157cf05c36be64c419f01690bd26a0db
Rath <niranjan.rath@sogeti.com>
2017-04-19 00:13:38 +0200

    first commit

---
+++ src/test/java/com/niranjan/selenium/TestBrowser.java
@@ -0,0 +1,31 @@
+package com.niranjan.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by NIRRATH on 18-4-2017.
+ */
+public class TestBrowser {
+    public WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void browseToGoogle() {
+        driver.get("http://www.tcs.com");
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+}
