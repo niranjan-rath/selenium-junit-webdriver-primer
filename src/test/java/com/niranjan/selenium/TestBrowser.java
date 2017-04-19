3cc8ecc927eca854a15593283993eba51114c033
Niranjan Rath <niranjan.rath@gmail.com>
2017-04-19 07:01:31 +0200

    removed By

--- src/test/java/com/niranjan/selenium/TestBrowser.java
+++ src/test/java/com/niranjan/selenium/TestBrowser.java
@@ -3,7 +3,6 @@ package com.niranjan.selenium;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
